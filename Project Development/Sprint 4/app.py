e42fe73674b2ef33b0e6f0e26c0f5316af5cc362
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2022-11-01 12:44:32 +0530

    Integrate model with the application
    Co-authored-by: Hariikon <109944329+Hariikon@users.noreply.github.com>

--- Project Development/Sprint 4/app.py
+++ Project Development/Sprint 4/app.py
@@ -1,16 +1,21 @@
 from flask import Flask,render_template,request
-
+from recognizer import recognize
 
 app=Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template("main.html")
+    return render_template("home.html")
+
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    
-      return render_template("predict.html")
+    if request.method=='POST':
+      image = request.files.get('photo', '')
+      best, others, img_name = recognize(image)
+      return render_template("predict.html", best=best, others=others, img_name=img_name)
+
+
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run()
